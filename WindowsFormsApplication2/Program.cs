13a50c4b04f11c8b736d82ec61f95d1d2427bd1a
Pavel Pochobut <pavel.pochobut@gmail.com>
2015-08-14 23:25:30 +0300

    initial commit

---
+++ WindowsFormsApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            new Thread(() =>
+            {
+                new Form1().ShowDialog();
+            }).Start();
+
+            Application.Run(new Form1());
+        }
+    }
+}
